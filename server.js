4b6eccf93789a630704a0a599c087131a6a75ed9
Chuong <chuong.hoanghuy@powergatesoftware.com>
2018-10-26 10:26:08 +0700

    ACROS

--- server.js
+++ server.js
@@ -17,10 +17,16 @@ firebase.initializeApp({
 
 app.use(express.static("./public")); 		// set the static files location /public/img will be /img for users
 app.use(morgan("dev")); // log every request to the console
-app.use(bodyParser.urlencoded({ "extended": "true" })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
 app.use(methodOverride("X-HTTP-Method-Override")); // override with the X-HTTP-Method-Override header in the request
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', "Content-Type");
+    next();
+});
 
 // routes ======================================================================
 require("./app/routes.js")(app);
